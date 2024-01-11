92c571959a35775a87eb7951a7ab105ce0a4ce32
Tiago Goulart <tiagogoulart3012@gmail.com>
2024-01-11 11:51:55 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int num1 = 10;
+        System.out.println(num1);
+        Integer num2 = Integer.valueOf(num1);
+        System.out.println(num2);
+    }
+}
